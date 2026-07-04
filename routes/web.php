0b03e781bd22a47ec9de9f35f7202e4c207554f5
ziad396 <tayelzeiad17@gmail.com>
2026-07-04 12:56:16 +0300

    updated

--- routes/web.php
+++ routes/web.php
@@ -38,31 +38,6 @@ Route::middleware(['auth', 'admin'])->controller(AdminController::class)->group(
     // Route::delete('adminpanal/delete/{id}', 'deleteExpense')->name('delete.expense');
      Route::delete('adminpanal/expense/delete/{expense}', 'deleteExpense')->name('delete.expense');
 });
-// Route::middleware(['auth', 'admin'])->controller(AdminController::class)->group(function () {
-//     // 👥 Manage Users
-//     Route::get('adminpanal/user', 'adminPanal')->name('adminpanal');
-//     Route::get('adminpanal/user/{user}/edit', 'edit')->name('update.user');
-//     Route::put('adminpanal/update/{user}', 'updateUser')->name('update.done');
-//     Route::get('adminpanal/user/insert', 'create')->name('insert.user'); // Fixed a typo here: 'use' to 'user'
-//     Route::post('adminpanal/user/addedd', 'insertUser')->name('insert.done');
-    
-//     // 💡 FIX: Added 'user' to the URL path so it is unique
-//     Route::delete('adminpanal/user/delete/{user}', 'deleteUser')->name('delete.user');
-
-//     // 💰 Manage Expense
-//     Route::get('manage/expense', 'createExpense')->name('expense.manage');
-//     Route::get('adminpanal/expense/{expense}/edit', 'editExpense')->name('update.expense');
-//     Route::put('adminpanal/expense/update/{expense}', 'updateExpense')->name('edit.done');
-    
-//     // 💡 FIX: Added 'expense' to the URL path so it is unique
-//     Route::delete('adminpanal/expense/delete/{expense}', 'deleteExpense')->name('delete.expense');
-// });
-// Route::get('create',function(){
-//     return ('create');
-// });
-// Route::get("/idea/{id}",function(){
-//     return ('update');
-// });
 
 
 
